47f3a61209b32bc18042a43c21376f9c9544b8ed
cynthiamaurice <cynthiamaurice9@gmail.com>
2023-04-25 16:59:40 -0700

    my files

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char);
+int _islower(int c);
+void print_alphabet_x10(void);
+int _abs(int);
+void jack_bauer(void);
+int add(int, int);
+void jack_bauer(void);
+void print_to_98(int n);
+void times_table(void);
+int _isalpha(int c);
+int print_sign(int n);
+int print_last_digit(int);
+void print_times_table(int n);;
+
+#endif
